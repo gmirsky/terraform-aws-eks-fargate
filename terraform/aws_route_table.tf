6914cd937d69eb5db625c19d7cf0f13c03fd8f7c
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 10:52:05 -0500

    added route tables

---
+++ terraform/aws_route_table.tf
@@ -0,0 +1,29 @@
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = merge(
+    var.tags,
+    {
+      Name = "private"
+    }
+  )
+}
+#
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(
+    var.tags,
+    {
+      Name = "public"
+    }
+  )
+}
+#
