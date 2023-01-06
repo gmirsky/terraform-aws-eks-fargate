dd073742feba8c2a0aceb760dac0d659963b24bf
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 14:15:42 -0500

    formatting

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,12 +6,12 @@ output "cli_command_output" {
 #
 output "aws_load_balancer_controller_role_arn" {
   description = "AWS load balancer ARN"
-  value = aws_iam_role.aws_load_balancer_controller.arn
-  sensitive = false
+  value       = aws_iam_role.aws_load_balancer_controller.arn
+  sensitive   = false
 }
 #
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
   description = "AWS VPC id"
-  sensitive = false
+  sensitive   = false
 }
