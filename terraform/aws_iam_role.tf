92a4371cb658fb41d8cdfa47b392d9e90f9c528c
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 11:31:00 -0500

    added additional iam role

--- terraform/aws_iam_role.tf
+++ terraform/aws_iam_role.tf
@@ -15,3 +15,22 @@ resource "aws_iam_role" "eks-cluster" {
 }
 POLICY
 }
+#
+resource "aws_iam_role" "eks-fargate-profile" {
+  name = "eks-fargate-profile"
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "eks-fargate-pods.amazonaws.com"
+          }
+        }
+      ]
+      Version = "2012-10-17"
+    }
+  )
+}
+#
