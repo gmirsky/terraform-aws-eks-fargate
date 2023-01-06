665253f9119fe558ae7226a178e0fa9c9f753a58
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 10:48:41 -0500

    added nat gateway

---
+++ terraform/aws_nat_gateway.tf
@@ -0,0 +1,14 @@
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
+  tags = merge(
+    var.tags,
+    {
+      Name = "nat"
+    }
+  )
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
+#
