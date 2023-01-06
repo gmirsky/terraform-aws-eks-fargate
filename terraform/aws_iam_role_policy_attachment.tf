c0b8e4fe48e482a28401963eaa0f78505989473a
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 10:57:18 -0500

    added iam policy role attachment

---
+++ terraform/aws_iam_role_policy_attachment.tf
@@ -0,0 +1,5 @@
+resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+#
