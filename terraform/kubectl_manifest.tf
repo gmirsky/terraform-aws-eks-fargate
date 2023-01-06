7daa31c3c587176d8f4e656ef432320be941421e
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 16:48:23 -0500

    added more kubectl commands

--- terraform/kubectl_manifest.tf
+++ terraform/kubectl_manifest.tf
@@ -1,8 +1,43 @@
-# 1- simple-deployment
+#
 resource "kubectl_manifest" "simple-deployment" {
   yaml_body = file("${path.module}/../k8s/simple-deployment.yaml")
   depends_on = [
     aws_eks_fargate_profile.staging
+  ]
+}
+#
+resource "kubectl_manifest" "service" {
+  yaml_body = file("${path.module}/../k8s/service.yaml")
+  depends_on = [
+    helm_release.metrics-server
+  ]
+}
+#
+
+resource "kubectl_manifest" "hpa" {
+  yaml_body = file("${path.module}/../k8s/hpa.yaml")
+  depends_on = [
+    helm_release.metrics-server,
+    kubectl_manifest.service
+  ]
+}
+#
+resource "kubectl_manifest" "pdb" {
+  yaml_body = file("${path.module}/../k8s/pdb.yaml")
+  depends_on = [
+    helm_release.metrics-server,
+    kubectl_manifest.service,
+    kubectl_manifest.hpa
+  ]
+}
+#
+resource "kubectl_manifest" "ingress" {
+  yaml_body = file("${path.module}/../k8s/ingress.yaml")
+  depends_on = [
+    helm_release.metrics-server,
+    kubectl_manifest.service,
+    kubectl_manifest.hpa,
+    kubectl_manifest.pdb
   ]
 }
 #
