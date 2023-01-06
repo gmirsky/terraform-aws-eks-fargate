e9a9589fa8d5f86683bc0ae2de3507f7708461cd
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 10:43:59 -0500

    added subnets

---
+++ terraform/aws_subnet.tf
@@ -0,0 +1,60 @@
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "${var.region}a"
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                      = "private-${var.region}a"
+      "kubernetes.io/role/internal-elb"           = "1"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    }
+  )
+}
+#
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "${var.region}b"
+
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                      = "private-${var.region}b"
+      "kubernetes.io/role/internal-elb"           = "1"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    }
+  )
+}
+#
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                      = "public-${var.region}a"
+      "kubernetes.io/role/elb"                    = "1"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    }
+  )
+}
+#
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                      = "public-${var.region}b"
+      "kubernetes.io/role/elb"                    = "1"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    }
+  )
+}
+#
