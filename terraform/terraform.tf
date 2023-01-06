b7ba21cd8f38afb4d6d6a40fe2c21bc6941218e0
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-06 13:41:23 -0500

    added provider for kubectl

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.49.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
   }
   required_version = ">= 1.0"
   # backend "s3" {
