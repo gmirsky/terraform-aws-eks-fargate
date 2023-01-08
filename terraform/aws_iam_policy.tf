d0169f81730d0a0c77af00c84be607173345a0a3
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-08 15:33:54 -0500

    added tfsec ignore

--- terraform/aws_iam_policy.tf
+++ terraform/aws_iam_policy.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "aws_load_balancer_controller" {
+  #tfsec:ignore:aws-iam-no-policy-wildcards
+  # policy file is downloaded from "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.4/docs/install/iam_policy.json"
   policy = file("./AWSLoadBalancerController.json")
   name   = "AWSLoadBalancerController"
   path   = "/"
