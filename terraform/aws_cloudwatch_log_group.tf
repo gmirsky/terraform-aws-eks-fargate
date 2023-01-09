e3e67e3ba6cd5cd4bb48f10e31bfaa8823893cb8
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-09 09:54:36 -0500

    added eks log group

--- terraform/aws_cloudwatch_log_group.tf
+++ terraform/aws_cloudwatch_log_group.tf
@@ -1,5 +1,13 @@
 resource "aws_cloudwatch_log_group" "vpc_main" {
-  name              = "vpc_main"
+  name              = "/aws/vpc/vpc_main"
+  skip_destroy      = false
+  retention_in_days = 90
+  kms_key_id        = aws_kms_key.cloudwatch_logs.key_id
+  tags              = local.tags
+}
+#
+resource "aws_cloudwatch_log_group" "eks_cluster" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
   skip_destroy      = false
   retention_in_days = 90
   kms_key_id        = aws_kms_key.cloudwatch_logs.key_id
