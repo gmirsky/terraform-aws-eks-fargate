a85b69566ad69745c722ae5c259d80804847c126
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-09 09:56:00 -0500

    added kms key

--- terraform/aws_kms_key.tf
+++ terraform/aws_kms_key.tf
@@ -4,5 +4,25 @@ resource "aws_kms_key" "cloudwatch_logs" {
   key_usage                = "ENCRYPT_DECRYPT"
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
   enable_key_rotation      = true
+  tags = merge(
+    {
+      Name = "cloudwatch_logs"
+    },
+    local.tags
+  )
+}
+#
+resource "aws_kms_key" "eks_secrets" {
+  description              = "KMS key for eks secrets"
+  deletion_window_in_days  = 7
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  enable_key_rotation      = true
+  tags = merge(
+    {
+      Name = "eks_secrets"
+    },
+    local.tags
+  )
 }
 #
