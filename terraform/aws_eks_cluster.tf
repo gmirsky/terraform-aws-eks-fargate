5bc857afef9964ba805b5e37d3c14fb1089ba8ef
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-09 09:55:06 -0500

    enabled cluster log types

--- terraform/aws_eks_cluster.tf
+++ terraform/aws_eks_cluster.tf
@@ -2,6 +2,13 @@ resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   version  = var.cluster_version
   role_arn = aws_iam_role.eks-cluster.arn
+  enabled_cluster_log_types = [
+    "api",
+    "authenticator",
+    "audit",
+    "scheduler",
+    "controllerManager"
+  ]
   vpc_config {
     endpoint_private_access = var.aws_eks_cluster_endpoint_private_access
     endpoint_public_access  = var.aws_eks_cluster_endpoint_public_access
@@ -15,16 +22,17 @@ resource "aws_eks_cluster" "cluster" {
       aws_subnet.public-b.id
     ]
   }
-  # encryption_config {
-  #   provider {
-  #     key_arn = aws_kms_key.eks-kms-key.arn
-  #   }
-  #   resources = ["secrets"]
-  # }
-  # depends upon may be deleted in the future when the AWS provider
-  # handles the deployment order as expected.
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.eks_secrets.arn
+    }
+    resources = [
+      "secrets"
+    ]
+  }
   depends_on = [
-    aws_iam_role_policy_attachment.amazon-eks-cluster-policy
+    aws_iam_role_policy_attachment.amazon-eks-cluster-policy,
+    aws_cloudwatch_log_group.eks_cluster
   ]
   tags = local.tags
 }
